9ca0da095b1fee2b036b7817762ee7527434d87d
Ludwig <ludwig.stumpp@unternehmertum.de>
2022-01-09 19:50:58 +0100

    Add function to load linear and nonlinear dataset

--- helpers/data.py
+++ helpers/data.py
@@ -1,1 +1,31 @@
 # module to load data files
+
+import pandas as pd
+
+
+def load_linear_dataset(filename: str = "data/linear_function_data.txt") -> pd.DataFrame:
+    """Load linear dataset.
+
+    Args:
+        filename (str, optional): Path to file. Defaults to "data/linear_function_data.txt".
+
+    Returns:
+        pd.DataFrame: Dataframe with x and y columns.
+    """
+    data = pd.read_csv(filename, sep=" ", header=None)
+    data.columns = ["x", "y"]
+    return data
+
+
+def load_nonlinear_dataset(filename: str = "data/nonlinear_function_data.txt") -> pd.DataFrame:
+    """Load nonlinear dataset.
+
+    Args:
+        filename (str, optional): Path to file. Defaults to "data/nonlinear_function_data.txt".
+
+    Returns:
+        pd.DataFrame: Dataframe with x and y columns.
+    """
+    data = pd.read_csv(filename, sep=" ", header=None)
+    data.columns = ["x", "y"]
+    return data

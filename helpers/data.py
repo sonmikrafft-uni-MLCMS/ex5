47e18d6c9a0264759a9071b4c5f3c385ebea2d5d
Sonja Krafft <ga83bin@mytum.de>
2022-01-10 18:14:41 +0100

    add part 1

--- helpers/data.py
+++ helpers/data.py
@@ -1,1 +1,16 @@
 # module to load data files
+
+import pandas as pd
+
+
+def load_dataset(filename: str = "data/linear_vectorfield_data_x0.txt") -> pd.DataFrame:
+    """Load linear dataset.
+    Args:
+        filename (str, optional): Path to file. Defaults to "data/linear_function_data.txt".
+    Returns:
+        pd.DataFrame: Dataframe with x and y columns.
+    """
+
+    data = pd.read_csv(filename, sep=" ", header=None)
+    data.columns = ["x", "y"]
+    return data

1342b7fd709ec68698b22e9907880ec150cf8229
Ludwig <ludwig.stumpp@unternehmertum.de>
2022-01-11 18:30:15 +0100

    Improve documentation

--- helpers/models.py
+++ helpers/models.py
@@ -8,31 +8,45 @@ class RidgeRegression:
     def __init__(self, lamb: float = 0):
         """Init method for Ridge Regression.
 
+        Finds a weight matrix W of shape (n_features, n_targets) that linearly maps from the feature space of
+        dimension n_features to the output space of dimension n_targets such that the L2 - norm between the
+        prediction and the target is minimized.
+
+        Ridge regression finds weights W such that:
+        ||y - XW||^2 = ||y - XW||^2 + lamb * ||W||^2
+        is minimized.
+
         See https://stats.stackexchange.com/questions/69205/how-to-derive-the-ridge-regression-solution for a
         mathematical derivation of the Ridge Regression normal form.
+
+        For an official implementation see:
+        https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.Ridge.html
+
+        Attributes:
+            lamb (float): Regularization parameter
+            W (np.ndarray): Weights of the model, of shape (n_features, n_targets)
 
         Args:
             lamb (float, optional): Regularization strength; must be a positive float. Defaults to 0.
         """
         self.lamb = lamb
-        self.w = None  # type: Optional[np.ndarray]
-        self.d = None  # type: Optional[int]
+        self.W = None  # type: Optional[np.ndarray]
 
-    def fit(self, X: np.ndarray, y: np.ndarray):
+    def fit(self, X: np.ndarray, Y: np.ndarray):
         """Fit the model to the data
 
         Fits the RidgeRegression model to the data and stores the weights in self.w.
 
         Args:
             X (np.ndarray): Training data, of shape (n_samples, n_features)
-            y (np.ndarray): Training targets, of shape (n_samples,)
+            Y (np.ndarray): Training targets, of shape (n_samples, n_targets)
         """
         if X.ndim == 1:
             X = X.reshape(-1, 1)  # add feature dimension
-        self.d = X.shape[1]
+        D = X.shape[1]
 
         # see normal form for ridge regression
-        self.w = np.linalg.lstsq(X.T.dot(X) + self.lamb * np.identity(self.d), X.T.dot(y), rcond=None)[0]
+        self.W = np.linalg.lstsq(X.T.dot(X) + self.lamb * np.identity(D), X.T.dot(Y), rcond=None)[0]
 
     def transform(self, X: np.ndarray) -> np.ndarray:
         """Calculate new prediction based on the trained model
@@ -41,34 +55,49 @@ class RidgeRegression:
             X (np.ndarray): Data to predict on, of shape (n_samples, n_features)
 
         Returns:
-            np.ndarray: Predictions, of shape (n_samples,)
+            np.ndarray: Predictions, of shape (n_samples, n_targets)
         """
-        assert self.w is not None, "Model not trained yet"
+        assert self.W is not None, "Model not trained yet."
         if X.ndim == 1:
             X = X.reshape(-1, 1)  # add feature dimension
-        return X @ self.w
+        return X @ self.W
 
-    def fit_transform(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def fit_transform(self, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
         """Fit the model and calculate predictions
 
         Args:
             X (np.ndarray): Training data, of shape (n_samples, n_features)
-            y (np.ndarray): Training targets, of shape (n_samples,)
+            y (np.ndarray): Training targets, of shape (n_samples, n_targets)
 
         Returns:
-            np.ndarray: Predictions, of shape (n_samples,)
+            np.ndarray: Predictions, of shape (n_samples, n_targets)
         """
-        self.fit(X, y)
+        self.fit(X, Y)
         return self.transform(X)
 
 
 class RBFRegression:
-    def __init__(self, eps, L, lamb: float = 0):
-        """Init method for Radial Basis Function
+    def __init__(self, eps, L, lamb: float = 0) -> None:
+        """Init method for RBF Regression.
+
+        First transforms the data (n_samples, n_features) into a design matrix Phi of dimension (n_samples, L),
+        then finds a weight matrix W of shape (L, n_targets) that linearly maps from the intermediate feature space
+        if dimension L to the output space of dimension n_targets such that the L2 - norm between the prediction and
+        the target is minimized.
+
+        RBF regression finds weights W such that:
+        ||y - PhiW||^2 = ||y - PhiW||^2 + lamb * ||W||^2
+        is minimized, where Phi is the design matrix and the intermediate feature space.
+
+        Note that the intermediate feature space is not learnt from the data, but is instead determined by the
+        bandwidth parameter eps and the number of RBFs L.
+
+        For an official implementation see:
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.Rbf.html
 
         Args:
             lamb (float, optional): Regularization strength; must be a positive float. Defaults to 0.
-            eps (float): bandwidth parameter (controls radius/flatness)
+            eps (float): bandwidth parameter (controls radius/flatness) of RBFs
             L (int): number of basis functions
         """
         self.eps = eps
@@ -79,55 +108,63 @@ class RBFRegression:
     def gaussian_rbf(self, x_l: np.ndarray, x: np.ndarray, eps: float) -> float:
         """Calculate the gaussian radial function for given vector pair
 
+        The gaussian radial function is defined as:
+        gaussian_rbf(x_l, x) = exp(-||x_l - x||^2 / (eps^2))
+
+        where:
+        - x_l: center point
+        - x: point to calculate the gaussian for
+        - eps: bandwidth parameter (controls radius/flatness)
+
         Args:
-            x_l (np.array): center vector of shape (D,)
-            x (np.array): data vector of shape (D,)
+            x_l (np.array): center of shape (n_features,)
+            x (np.array): incoming data of shape (n_features,)
             eps (float): bandwidth parameter (controls radius/flatness) shape (1,)
 
         Returns:
-            [float]: radial basis function value, shape (1,)
+            float: value for gaussian radial function, shape (1,)
         """
         return np.exp(-np.linalg.norm(x_l - x, axis=-1) ** 2 / (eps ** 2))
 
     def sample_interpolation_center(self, X: np.ndarray) -> np.ndarray:
-        """randomly sample L number of dataset points
+        """randomly sample L number of dataset points.
 
         Args:
-            X (np.ndarray): dataset
+            X (np.ndarray): dataset of shape (n_samples, n_features)
 
         Returns:
-            [np.ndarray]: matrix with L Number of dataset point pairs
+            np.ndarray: matrix with L dataset point pairs of shape (L, n_features)
         """
         random_interpolation_centers = np.random.choice(X[:, 0], size=self.L)
         return random_interpolation_centers
 
     def calculate_design_matrix(self, X: np.ndarray, interpolation_centers: np.ndarray, eps: float) -> np.ndarray:
-        """Fill the designmatrix by applying rbf function on X at centers
+        """Fill the designmatrix by applying rbf function on X at centers.
 
         Args:
-            X (np.ndarray): dataset add shapes !!!!
-            interpolation_centers (np.ndarray): sampled approximation points (num=L)
-            eps (float): bandwidth parameter (controls radius/flatness)
+            X (np.ndarray): dataset of shape (n_samples, n_features)
+            interpolation_centers (np.ndarray): interpolation centers for RBFs of shape (L, n_features)
+            eps (float): bandwidth parameter (controls radius/flatness) shape (1,)
 
         Returns:
-            Phi (np.ndarray): Design matrix Phi
+            np.ndarray: Design matrix Phi of shape (n_samples, L) where L is the number of RBFs to use
         """
         shape = (X.shape[0], self.L)
-        Phi = np.zeros(shape)  # NxL
+        Phi = np.zeros(shape)  # shape (n_samples, L)
 
         for idx_centers, center in enumerate(interpolation_centers):
             Phi[:, idx_centers] = self.gaussian_rbf(center, X, eps)
 
         return Phi
 
-    def fit(self, X: np.ndarray, y: np.ndarray):
+    def fit(self, X: np.ndarray, Y: np.ndarray) -> None:
         """Fit the model to the data
 
-        Fits the RBF Regression model to the data and stores the weights in self.w.
+        Fits the RBF Regression model to the data.
 
         Args:
             X (np.ndarray): Training data, of shape (n_samples, n_features)
-            y (np.ndarray): Training targets, of shape (n_samples,)
+            y (np.ndarray): Training targets, of shape (n_samples, n_targets)
         """
         if X.ndim == 1:
             X = X.reshape(-1, 1)  # add feature dimension
@@ -140,16 +177,16 @@ class RBFRegression:
         Phi = self.calculate_design_matrix(X, self.interpolation_centers, self.eps)
 
         # find optimal w* for design matrix Phi
-        self.linear_regression.fit(Phi, y)
+        self.linear_regression.fit(Phi, Y)
 
     def transform(self, X: np.ndarray) -> np.ndarray:
-        """Calculate new prediction based on the trained model
+        """Calculate new prediction based on the trained model.
 
         Args:
             X (np.ndarray): Data to predict on, of shape (n_samples, n_features)
 
         Returns:
-            np.ndarray: Predictions, of shape (n_samples,)
+            np.ndarray: Predictions, of shape (n_samples, n_targets)
         """
         assert self.interpolation_centers is not None, "Model not trained yet"
         if X.ndim == 1:
@@ -158,15 +195,15 @@ class RBFRegression:
         Phi = self.calculate_design_matrix(X, self.interpolation_centers, self.eps)
         return self.linear_regression.transform(Phi)
 
-    def fit_transform(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """Fit the model and calculate predictions
+    def fit_transform(self, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+        """Fit the model and calculate predictions.
 
         Args:
             X (np.ndarray): Training data, of shape (n_samples, n_features)
-            y (np.ndarray): Training targets, of shape (n_samples,)
+            y (np.ndarray): Training targets, of shape (n_samples, n_targets)
 
         Returns:
-            np.ndarray: Predictions, of shape (n_samples,)
+            np.ndarray: Predictions, of shape (n_samples, n_targets)
         """
-        self.fit(X, y)
+        self.fit(X, Y)
         return self.transform(X)

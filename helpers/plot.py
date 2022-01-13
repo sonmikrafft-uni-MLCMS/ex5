41f7854faa684678cdf7dd14f5ef2ef840ffacd1
Sonja Krafft <ga83bin@mytum.de>
2022-01-13 18:23:26 +0100

    refactor code and complete documentation for task 2

--- helpers/plot.py
+++ helpers/plot.py
@@ -1,1 +1,26 @@
 # module to plot data
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_phase_portrait(
+    mesh_tuple: np.ndarray,
+    flow_tuple: np.ndarray,
+    **kwargs,
+):
+    """
+    Plot phase portrait.
+    Args:
+        mesh_tuple (np.ndarray): 2D mesh with two elements of shape (N,)
+        flow_tuple (np.ndarray): 2D flow tuple of two elements of shape (N,)
+        **kwargs (): additional arguments
+    """
+    (X1, X2) = mesh_tuple
+    (U, V) = flow_tuple
+
+    _, ax = plt.subplots(1, 1)
+
+    ax.streamplot(X1, X2, U, V, density=1.0, **kwargs)
+    ax.set_aspect("equal")
+    ax.set_xlim([X1[0, 0], X1[-1, -1]])
+    ax.set_ylim([X2[0, 0], X2[-1, -1]])
---
+++ helpers/vectorfields.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+from scipy.integrate import odeint
+
+
+def compute_finite_difference(x_0: pd.DataFrame, x_1: pd.DataFrame, delta_t: float = 0.1) -> pd.DataFrame:
+    """
+    computes finite difference between x_0 and x_1 for a given delta_t
+
+    Args:
+        x_0 (pd.DataFrame): points of shape (N,D) at time t=0 of
+        x_1 (pd.DataFrame): points of shape (N,D) at time delta_t
+        delta_t (float): delta of time
+
+    Returns: v_k, approximation of the vector field
+    """
+    v_k = x_1 - x_0
+    v_k /= delta_t
+    return v_k
+
+
+def compute_closed_form_linear_supervised_learning(x: pd.DataFrame, f: pd.DataFrame) -> pd.DataFrame:
+    """
+    computes closed form solution A.T for linear functions that minimizes the least-squares error
+    Args:
+        x (pd.DataFrame): input of shape (N,D)
+        f (pd.DataFrame): f \approx Ax of shape (N,D)
+
+    Returns: A.T of shape (D,D), closed form solution
+    """
+
+    x_t = x.T
+    cov = x_t @ x
+    cov_inv = pd.DataFrame(np.linalg.pinv(cov.values), cov.columns, cov.index)
+
+    result = cov_inv @ x_t
+    result = result @ f
+
+    return result
+
+def deriv(x: np.ndarray, t: np.ndarray, A_T: np.ndarray, dim1: int, dim2: int) -> np.ndarray:
+    """
+    Args:
+        x (np.ndarray): input of shape (N,D)
+        t (np.ndarray): time interval
+        A (np.ndarray): matrix of shape (D,D)
+        dim1 (int): N
+        dim2 (int): D
+
+    Returns: x_dot of shape (N,D), x_dot = Ax
+
+    """
+    x = x.reshape(dim1, dim2)
+    x_dot = x @ A_T
+    return x_dot.reshape(-1)
+
+def solve_ode(t: np.array, A_T: pd.DataFrame, x: pd.DataFrame) -> pd.DataFrame:
+    """
+    Args:
+        A (pd.DataFrame): matrix of shape (D,D)
+        x (pd.DataFrame): input of shape (N,D)
+
+    Returns: solution to differential equation x_dot = Ax for a given input x and matrix A
+
+    """
+    dim1 = x.shape[0]
+    dim2 = x.shape[1]
+    initial = x.to_numpy().reshape(-1)
+    sol = odeint(deriv, initial, t, args=(A_T.to_numpy(), dim1, dim2))
+    return sol
+
+
+def mean_squared_error(x_head: np.ndarray, x: np.ndarray) -> float:
+    """
+    Args:
+        x_head (np.ndarray): approximated solution of shape (N,D)
+        x (np.ndarray): observed input of shape (N,D)
+
+    Returns: Mean Squared Error
+
+    """
+    result = x_head - x
+    result = result ** 2
+    return result.mean()
+
